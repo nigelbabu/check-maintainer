b0a78a2a64ad2e94edf5726a618dec35366cb6d7
Nigel Babu <nigelb@redhat.com>
2016-08-05 18:19:20 +0530

    Add more testing

--- tests.py
+++ tests.py
@@ -8,6 +8,9 @@ class TestMaintainerReview(unittest.TestCase):
     @mock.patch('check.get_reviewers')
     @mock.patch('check.changed_paths')
     def test_non_maintainer(self, paths, reviewers):
+        '''
+        Only reviewer for a path is a non-maintainer.
+        '''
         paths.return_value = [
                 'xlators/cluster/afr/src/afr-common.c',
         ]
@@ -17,6 +20,9 @@ class TestMaintainerReview(unittest.TestCase):
     @mock.patch('check.get_reviewers')
     @mock.patch('check.changed_paths')
     def test_maintainer(self, paths, reviewers):
+        '''
+        Reviewer for a path is a maintainer.
+        '''
         paths.return_value = [
                 'xlators/cluster/afr/src/afr-common.c',
         ]
@@ -26,8 +32,50 @@ class TestMaintainerReview(unittest.TestCase):
     @mock.patch('check.get_reviewers')
     @mock.patch('check.changed_paths')
     def test_architect(self, paths, reviewers):
-        changed_paths.return_value = [
+        '''
+        Top-level architect has reviewed.
+        '''
+        paths.return_value = [
                 'xlators/cluster/afr/src/afr-common.c',
         ]
         reviewers.return_value = ['jdarcy@redhat.com']
         self.assertEqual(check_maintainer_review(), (True, 'Architect Review'))
+
+    @mock.patch('check.get_reviewers')
+    @mock.patch('check.changed_paths')
+    def test_path_not_specified(self, paths, reviewers):
+        '''
+        Changed paths are not part of the pre-defined set of paths.
+        '''
+        paths.return_value = [
+                'foo/bar/',
+        ]
+        reviewers.return_value = ['non-maintainer@redhat.com']
+        self.assertEqual(check_maintainer_review(), None)
+
+    @mock.patch('check.get_reviewers')
+    @mock.patch('check.changed_paths')
+    def test_two_paths_one_acked(self, paths, reviewers):
+        '''
+        Two paths are defined and only one has maintainer ack.
+        '''
+        paths.return_value = [
+                'xlators/cluster/afr/src/afr-common.c',
+                'xlators/features/bit-rot/src/bitd/bit-rot.c'
+        ]
+        reviewers.return_value = ['pkarampu@redhat.com']
+        self.assertEqual(check_maintainer_review(), False)
+
+    @mock.patch('check.get_reviewers')
+    @mock.patch('check.changed_paths')
+    def test_two_paths_one_acked_one_undefined(self, paths, reviewers):
+        '''
+        One of two paths is undefined, and the other has maintainer ack.
+        '''
+        paths.return_value = [
+                'xlators/cluster/afr/src/afr-common.c',
+                'foo/bar'
+        ]
+        reviewers.return_value = ['pkarampu@redhat.com']
+        self.assertEqual(check_maintainer_review(), True)
+
